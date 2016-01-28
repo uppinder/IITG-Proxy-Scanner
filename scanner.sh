5696b3ed3c79a32bb0d679da59d9841788278272
uppinder <uppinderchugh@gmail.com>
2016-01-28 19:18:31 +0530

    Progress % and nicer output

--- scanner.sh
+++ scanner.sh
@@ -3,7 +3,9 @@
 #create text file to store working proxies
 touch proxies.txt
 
-echo "Scanning..."
+> proxies.txt
+
+counter=0
 
 #add more ports if required 
 ports="3128 808 8080"
@@ -15,11 +17,16 @@ do
 		do
 			http_proxy="http://172.16.${i}.${j}:${port}"
 			#echo "Checking ${http_proxy}"
-			wget -q --spider -e use_proxy=yes -e http_proxy=$http_proxy -q --tries=1 --timeout=0.3 --spider http://google.com
-
+			wget -q --spider -e use_proxy=yes -e http_proxy=$http_proxy -q --tries=1 --timeout=0.1 --spider http://google.com
+			
 			if [ $? -eq 0 ]; then
-				echo "${http_proxy}" > proxies.txt
+				echo "${http_proxy:7}" >> proxies.txt
 			fi
+
+			(( counter++ ))
+			percent=$((counter / 18))
+			echo -ne "Scanning... ${percent}% done\r"
+			
 		done
 	done
 done
